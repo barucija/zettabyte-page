1e3379a23a50b957ff8293a9ecf3188f89795478
Emir Barucija <emchemir@gmail.com>
2022-05-26 20:12:47 +0200

    FirstPush

---
+++ script.js
@@ -0,0 +1,44 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 30, 2022 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="timer"
+  document.getElementById("timer-days").innerHTML = days;
+  document.getElementById("descroptionTimerDays").innerHTML =  " dana";
+
+  document.getElementById("timer-space").innerHTML = " : ";
+
+  document.getElementById("timer-hours").innerHTML = hours;
+  document.getElementById("descroptionTimerHours").innerHTML =  " sati";
+
+  document.getElementById("timer-space2").innerHTML = " : ";
+
+  document.getElementById("timer-minutes").innerHTML = minutes;
+  document.getElementById("descroptionTimerMinutes").innerHTML =  " minuta";
+
+  document.getElementById("timer-space3").innerHTML = " : ";
+
+  document.getElementById("timer-seconds").innerHTML = seconds;
+  document.getElementById("descroptionTimerSeconds").innerHTML =  " sekundi";
+  
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
