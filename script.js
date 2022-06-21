b65e035e80c0f7ce7150598126c12a9aa429398a
Emir Barucija <emchemir@gmail.com>
2022-06-21 15:37:51 +0200

    prijaveZatvorene

--- script.js
+++ script.js
@@ -17,22 +17,22 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="timer"
-  document.getElementById("timer-days").innerHTML = days;
+  document.getElementById("timer-days").innerHTML = "00";
   document.getElementById("descroptionTimerDays").innerHTML =  " dani";
 
   document.getElementById("timer-space").innerHTML = " : ";
 
-  document.getElementById("timer-hours").innerHTML = hours;
+  document.getElementById("timer-hours").innerHTML = "00";
   document.getElementById("descroptionTimerHours").innerHTML =  " sati";
 
   document.getElementById("timer-space2").innerHTML = " : ";
 
-  document.getElementById("timer-minutes").innerHTML = minutes;
+  document.getElementById("timer-minutes").innerHTML = "00";
   document.getElementById("descroptionTimerMinutes").innerHTML =  " minute";
 
   document.getElementById("timer-space3").innerHTML = " : ";
 
-  document.getElementById("timer-seconds").innerHTML = seconds;
+  document.getElementById("timer-seconds").innerHTML = "00";
   document.getElementById("descroptionTimerSeconds").innerHTML =  " sekunde";
   
   // If the count down is over, write some text 
